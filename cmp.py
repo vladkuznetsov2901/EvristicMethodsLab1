c49a5fa4fc190fd42fd760aadd75f70064d93b2a
vladkuznetsov2901 <vladkuznetsov2901@gmail.com>
2024-02-20 21:11:01 +0300

    HUIHUIHUIHUIHUIHUIHUIHUIHUIHUIHUIHUI

--- cmp.py
+++ cmp.py
@@ -11,3 +11,13 @@ def minimal_load_processor(arr_processors: list):
             min_load_processor = sum_of_processor(arr_processors[i])
             index = i
     return index
+
+
+def minimal_load_processor_hdmt(arr_processors: list, start, end):
+    index = 0
+    min_load_processor = sum_of_processor(arr_processors[0])
+    for i in range(start, end):
+        if sum_of_processor(arr_processors[i]) < min_load_processor:
+            min_load_processor = sum_of_processor(arr_processors[i])
+            index = i
+    return index
--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from cmp import *
 cnt_processors = int(input("Enter count of processors: "))
 a = int(input("Enter value from: "))
 b = int(input("Enter value to: "))
-N = int(input("Enter the count of columns: "))
 
 rows = random.randint(a, b)
 columns = 2
@@ -30,8 +29,12 @@ for i in range(len(T)):
     index = minimal_load_processor(arr_processors_random)
     arr_processors_random[index].append(T[i])
 
+_max = sum_of_processor(arr_processors_random[0])
 for i in range(len(arr_processors_random)):
-    print(f"p{i + 1} =", arr_processors_random[i])
+    if sum_of_processor(arr_processors_random[i]) > _max:
+        _max = sum_of_processor(arr_processors_random[i])
+    print(f"p{i + 1} =", arr_processors_random[i], "Sum:", sum_of_processor(arr_processors_random[i]))
+print("Max:", _max)
 
 T.sort()
 
@@ -74,22 +77,51 @@ for i in range(len(T)):
     index = minimal_load_processor(arr_processors_sort_down)
     arr_processors_sort_down[index].append(T[i])
 
+_max = sum_of_processor(arr_processors_sort_down[0])
 for i in range(len(arr_processors_sort_down)):
-    print(f"p{i + 1} =", arr_processors_sort_down[i])
+    if sum_of_processor(arr_processors_sort_down[i]) > _max:
+        _max = sum_of_processor(arr_processors_sort_down[i])
+    print(f"p{i + 1} =", arr_processors_sort_down[i], "Sum:", sum_of_processor(arr_processors_sort_down[i]))
+print("Max:", _max)
 
 print()
 print("################################################")
 print()
 
+is_even = False
+
+while not is_even:
+    if cnt_processors % 2 == 0:
+        is_even = True
+    else:
+        print("Enter the even count")
+        cnt_processors = int(input("Enter count of processors: "))
+
 print("T:", T)
+
+for i in range(len(arr_processors_sort_down)):
+    print(f"p{i + 1} =", arr_processors_sort_down[i])
 
 arr_subprocesses = []
 
 for i in range(cnt_processors * 2):
     arr_subprocesses.append([])
 
-for i in range(len(T)):
-    minimal_load_processor_value = minimal_load_processor(arr_subprocesses)
+start_index = 0
+end = start_index + cnt_processors // 2
+for i in range(len(arr_processors_sort_down)):
+    for k in range(len(arr_processors_sort_down[i])):
+        if end > len(arr_subprocesses):
+            break
+        index = minimal_load_processor_hdmt(arr_subprocesses, start_index,
+                                            end)
+        arr_subprocesses[index].append(arr_processors_sort_down[i][k])
+    start_index += cnt_processors // 2
+    end = start_index + cnt_processors // 2
+
+print()
+print("################################################")
+print()
 
 for i in range(len(arr_subprocesses)):
-    print(f"p{i + 1} =", arr_subprocesses[i])
+    print(f"p{i + 1} =", arr_subprocesses[i], "Sum:", sum_of_processor(arr_subprocesses[i]))
